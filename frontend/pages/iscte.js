11b688df5176005fd314557c8fab2e4aa0593180
Rodrigo Simões <rodjbs@gmail.com>
2023-06-02 18:37:25 +0100

    Adjust elevation sizes for iscte instance

--- frontend/pages/iscte.js
+++ frontend/pages/iscte.js
@@ -1,9 +1,9 @@
 import Page from '@/components/Page'
 
 const prismSizes = [
-  {caption: "Small", size: 500},
-  {caption: "Medium", size: 1000},
-  {caption: "Large", size: 1500}
+  {caption: "Small", size: 400},
+  {caption: "Medium", size: 800},
+  {caption: "Large", size: 1200}
 ];
 
 const defaultPrismSize = prismSizes[1];
@@ -16,9 +16,9 @@ export default function Home() {
       hasDensity={false}
       backendUrl="http://localhost:5001"
       measurements={[
-          {name: "all", description: "all", unit: "devices", max: 1200},
-          {name: "only_randoms", description: "only_randoms", unit: "devices", max: 800},
-          {name: "no_randoms", description: "no_randoms", unit: "devices", max: 500}]}
+          {name: "all", description: "all", unit: "devices", max: 748},
+          {name: "only_randoms", description: "only_randoms", unit: "devices", max: 655},
+          {name: "no_randoms", description: "no_randoms", unit: "devices", max: 101}]}
       prismSizes={prismSizes}
       defaultPrismSize={defaultPrismSize}
       hasLive={true}
