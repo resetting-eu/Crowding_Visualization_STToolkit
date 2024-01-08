f06fc9b58e62bbabc48269bfce880ecc79cfa5d9
Rodrigo Simões <rodjbs@gmail.com>
2024-01-08 21:51:42 +0000

    Add unit test for nextLocalMaxIndex

--- frontend/__tests__/utils.test.js
+++ frontend/__tests__/utils.test.js
@@ -1,6 +1,6 @@
 const { expect, test } = require('@jest/globals');
 
-import { concatDataIndexes } from 'Utils';
+import { concatDataIndexes, nextLocalMaxIndex } from 'Utils';
 
 test("concatDataIndexes test", () => {
     expect(concatDataIndexes(5, 1, 20)).toStrictEqual({first_old: 0, first_new: 0});
@@ -9,4 +9,9 @@ test("concatDataIndexes test", () => {
     expect(concatDataIndexes(19, 2, 20)).toStrictEqual({first_old: 1, first_new: 0});
     expect(concatDataIndexes(19, 25, 20)).toStrictEqual({first_old: 19, first_new: 5});
     expect(concatDataIndexes(25, 25, 20)).toStrictEqual({first_old: 25, first_new: 5});
+});
+
+test("nextLocalMaxIndex test", () => {
+    expect(nextLocalMaxIndex([0, 1, 0], -1)).toBe(1);
+    expect(nextLocalMaxIndex([0, 1, 0], 1)).toBe(null);
 });
