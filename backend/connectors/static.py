e0eea6aba74f480c494a2836c5c9512a19207105
Rodrigo Simões <rodjbs@gmail.com>
2024-01-12 17:47:07 +0000

    Static connector: keep file in memory

--- backend/connectors/static.py
+++ backend/connectors/static.py
@@ -1,8 +1,11 @@
 import json
+import sys
 
 def generate_handler(parameters):
     path = parameters["path"]
+    print(f"Loading {path}", file=sys.stderr)
+    with open(path, encoding="utf-8") as f:
+        res = json.load(f)
     def static_handler(_):
-        with open(path, encoding="utf-8") as f:
-            return json.load(f)
+        return res
     return static_handler
