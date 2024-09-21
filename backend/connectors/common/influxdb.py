e6e6fa316f6861fa2eb87206f97a0bed2f3d8880
Rodrigo Simões <rodjbs@gmail.com>
2024-09-21 18:09:17 +0100

    Backend live endpoint reimplementation
    Now we return a whole batch of values instead of only the values that are after a certain timestamp

--- backend/connectors/common/influxdb.py
+++ backend/connectors/common/influxdb.py
@@ -1,5 +1,6 @@
 import influxdb_client
 from time import perf_counter
+from .utils import array_put_at, array_pad
 import sys
 
 def query_range_str(bucket, start, end, every, locations, location_variable, filters):
@@ -16,7 +17,20 @@ def query_range_str(bucket, start, end, every, locations, location_variable, fil
         res += '|> aggregateWindow(every: ' + every + ', fn: mean, createEmpty: false)'
     return res
 
-def append_value(values, record, metric_variable, location_variable):
+def query_last_str(bucket, start, locations, location_variable, filters):
+    # TODO usar bind parameters em vez de espetar os params diretamente na query string
+    res = 'from(bucket: "' + bucket + '")\
+        |> range(start: ' + start + ')'
+    if filters:
+        for filter in filters:
+            res += '|> filter(fn: ' + filter + ')'
+    if locations:
+        locations_regex = '|'.join(map(lambda l: "^" + str(l) + "$", locations))
+        res += '|> filter(fn: (r) => r["' + location_variable + '"] =~ /' + locations_regex + '/)'
+    res += '|> last() |> group() |> keep(columns: ["_time"])'
+    return res
+
+def put_value(values, record, metric_variable, location_variable, index):
     location_id = record.values[location_variable]
     value = record.values["_value"]
 
@@ -28,11 +42,11 @@ def append_value(values, record, metric_variable, location_variable):
         metric_obj = values[metric]
         if location_id not in metric_obj:
             metric_obj[location_id] = []
-        metric_obj[location_id].append(value)
+        array_put_at(metric_obj[location_id], index, value)
     else:
         if location_id not in values:
             values[location_id] = []
-        values[location_id].append(value)
+        array_put_at(values[location_id], index, value)
 
 def query(url, token, org, bucket, start, end, location_variable, metric_variable=None, every=None, locations=[], filters=[]):
     with influxdb_client.InfluxDBClient(url=url, token=token, org=org, debug=False) as client:
@@ -48,6 +62,25 @@ def query(url, token, org, bucket, start, end, location_variable, metric_variabl
             for record in table.records:
                 timestamp = record.values["_time"].isoformat().replace("+00:00", "Z")
                 timestamps.add(timestamp)
-                append_value(values, record, metric_variable, location_variable)
-    sorted_timestamps = sorted(timestamps)
-    return {"timestamps": sorted_timestamps, "values": values}
+                index = sorted(timestamps).index(timestamp)
+                put_value(values, record, metric_variable, location_variable, index)
+    pad_values(values, len(timestamps))
+    return {"timestamps": sorted(timestamps), "values": values}
+
+def query_last_timestamp(url, token, org, bucket, start, location_variable, locations=[], filters=[]):
+    with influxdb_client.InfluxDBClient(url=url, token=token, org=org, debug=False) as client:
+        query_api = client.query_api()
+        query_str = query_last_str(bucket, start, locations, location_variable, filters)
+        start_time = perf_counter()
+        tables = query_api.query(query_str)
+        end_time = perf_counter()
+        print("query time: {}".format(end_time - start_time), file=sys.stderr)
+        for table in tables:
+            for record in table.records:
+                return record.values["_time"].isoformat().replace("+00:00", "Z")
+    raise Exception("No data in range")
+
+def pad_values(values, length):
+    for metric_name in values:
+        for location in values[metric_name]:
+            array_pad(values[metric_name][location], length)
--- backend/connectors/influxdb_live.py
+++ backend/connectors/influxdb_live.py
@@ -1,10 +1,6 @@
-from .common.influxdb import query
-from .common.utils import parse_duration, parse_date, dt_to_string, uuid
-from .common.data import empty_response
+from .common.influxdb import query, query_last_timestamp
+from .common.utils import parse_duration
 from datetime import datetime
-
-# maps client_id to last obtained timestamp
-clients_last_timestamp = {}
 
 def generate_handler(parameters):
     url = parameters["url"]
@@ -14,41 +10,14 @@ def generate_handler(parameters):
     metric_variable = parameters.get("metric_variable")
     location_variable = parameters["location_variable"]
     filters = parameters.get("filters")
-    initial_time_offset = parameters["initial_time_offset"]
+    offset = parameters["offset"]
+    offset_td = parse_duration(offset)
     interval = parameters["interval"]
-    interval_td = parse_duration(interval)
-    def influxdb_live_handler(args):
-        client_id = args.get("client_id")
-        last_timestamp = clients_last_timestamp.get(client_id)
-        if not last_timestamp:
-            initial_time_offset_td = parse_duration(initial_time_offset)
-            start_dt = datetime.utcnow() - initial_time_offset_td
-            start = dt_to_string(start_dt)
-            res = query(url, token, org, bucket, start, None, location_variable, metric_variable, interval, None, filters)
-            discard_last_timestamp(res)
-            client_id = uuid()
-            res["client_id"] = client_id
-            clients_last_timestamp[client_id] = res["timestamps"][-1]
-        else:
-            last_timestamp_dt = parse_date(last_timestamp)
-            if last_timestamp_dt + interval_td > datetime.utcnow():
-                res = empty_response
-            else:
-                start = last_timestamp
-                res = query(url, token, org, bucket, start, None, location_variable, metric_variable, interval, None, filters)
-                keep_first_timestamp(res)
-                clients_last_timestamp[client_id] = res["timestamps"][0]
+    def influxdb_live_handler(_):
+        last_timestamp_in_bucket = query_last_timestamp(url, token, org, bucket, "-30d", location_variable, None, filters)
+        last_dt_in_bucket = datetime.fromisoformat(last_timestamp_in_bucket.replace("Z", "+00:00"))
+        start_dt = last_dt_in_bucket - offset_td
+        start = start_dt.isoformat()
+        res = query(url, token, org, bucket, start, None, location_variable, metric_variable, interval, None, filters)
         return res
     return influxdb_live_handler
-
-def keep_first_timestamp(res):
-    res["timestamps"] = [res["timestamps"][0]]
-    for metric in res["values"]:
-        for location in res["values"][metric]:
-            res["values"][metric][location] = [res["values"][metric][location][0]]
-
-def discard_last_timestamp(res):
-    res["timestamps"] = res["timestamps"][:-1]
-    for metric in res["values"]:
-        for location in res["values"][metric]:
-            res["values"][metric][location] = res["values"][metric][location][:-1]
